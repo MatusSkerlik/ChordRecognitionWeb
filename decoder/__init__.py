d82914d47800c51b4d87641f1bb40e7aa69073f6
MatusSkerrlik <hernyucet11@gmail.com>
2020-05-06 19:08:00 +0200

    deleted decoder.py

--- decoder/__init__.py
+++
@@ -1,2 +0,0 @@
-from decoder.decoder import (Stream, TerminatedStream, Stack, IntegerStack, StreamPointer, pitchname, natural, digit,
-                             degree, shorthand, modifier, interval, decrypt, ilist)
